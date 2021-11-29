3ba5aaef094b5fa080a3855aee11265b3a1430ed
Miguel Lima <miguellimaoliver97@gmail.com>
2021-11-29 19:08:45 -0300

    Exercicio 3

---
+++ exercicios.js-Day1/exercicio-3.js
@@ -0,0 +1,11 @@
+let a = 45
+let b = 456
+let c = 2111
+
+if (a > b && a > c) {
+    console.log('"A" é maior que "B" e "C"');
+} else if (b > a && b > c) {
+    console.log('"B" é maior que "A" e "C"');
+} else {
+    console.log('"C" é maior')
+};
