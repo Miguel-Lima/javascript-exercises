16bed0e62bb115aebc43c76442d7377cceea277a
Miguel Lima <miguellimaoliver97@gmail.com>
2022-01-26 20:56:41 -0300

    Functions js

---
+++ Curso de Javascript/functions.js
@@ -0,0 +1,31 @@
+// Estrutura padrão de uma function
+let corSite = "azul";
+
+function resetaCor() {
+    corSite = "";
+}
+
+resetaCor(); // chamando a function
+console.log(corSite) // imprimindo o resultado
+
+// Resetando uma unica cor em uma function
+
+let corSite1 ="verde";
+
+function mudarCor1(cor){ //parametro cor
+    corSite1 = cor; // colocando ele com o valor do Let 
+}
+mudarCor1("vermelho"); // mudando a sua cor chamando a function
+console.log(corSite1) // imprimindo resultado
+
+// Mudando a cor utilizando o parâmetro para uma outra cor
+
+let corSite2 = "branco";
+
+function mudaCor2(cores, tonalidades){
+    mudaCor2 = cores + ' ' + tonalidades;
+}
+
+console.log(corSite2);
+mudaCor2("verde", "claro")
+console.log(mudaCor2)
