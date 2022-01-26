9e889f53a4943fe789c6279d90dda30c1ed52ccc
Miguel Lima <miguellimaoliver97@gmail.com>
2022-01-26 20:33:58 -0300

    Tipos primitivos

---
+++ Curso de Javascript/primitivos.js
@@ -0,0 +1,5 @@
+let nome = 'Miguel Lima'; // string literal
+let idade = 24; // number literal
+let estaAprovado = true; // boolean
+let semDefinicao ; // undefined
+let corSelecionada = null // redefinir um valor
