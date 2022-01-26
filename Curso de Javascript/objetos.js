c233644c969251a7890ec615e292ca1b66979e9f
Miguel Lima <miguellimaoliver97@gmail.com>
2022-01-26 20:42:45 -0300

    Objetos js

---
+++ Curso de Javascript/objetos.js
@@ -0,0 +1,15 @@
+let nome = 'Miguel Lima'; // string literal
+let idade = 24; // number literal
+let estaAprovado = true; // boolean
+let semDefinicao ; // undefined
+let corSelecionada = null // redefinir um valor
+
+let pessoa = {
+    nome: 'Miguel',
+    idade: 24,
+    estaAprovado: true,
+    aluno: 'Trybe',
+    desenvolvimento: 'Full Stack'
+}
+
+console.log(pessoa)
